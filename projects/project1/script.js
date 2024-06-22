1fce9a5969304a257dd43e350bbbd692d570d0b7
Dushyant2026 <dushyantpandit799@gmail.com>
2024-06-23 02:01:51 +0530

    mark all my changes

--- projects/project1/script.js
+++ projects/project1/script.js
@@ -1,2 +1,63 @@
 // console.log("TO CHECK THAT PROPER WORK OF SCRIPT")
 
+
+const temp = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+temp.forEach(function (button){
+  button.addEventListener('click' , function(nums){
+
+
+                                                                           // simple project just add an event (click) 
+
+    // if(nums.target.id === "grey"){                                     // we can use if-else as well as switch case 
+    //   body.style.backgroundColor = nums.target.id;
+    // }
+    // if(nums.target.id === "black"){
+    //   body.style.backgroundColor = nums.target.id;
+    // }
+    // if(nums.target.id === "blue"){
+    //   body.style.backgroundColor = nums.target.id;
+    // }
+    // if(nums.target.id === "yellow"){
+    //   body.style.backgroundColor = nums.target.id;
+    // }
+    // if(nums.target.id === "red"){
+    //   body.style.backgroundColor = nums.target.id;
+    // }
+    // if(nums.target.id === "pink"){
+    //   body.style.backgroundColor = nums.target.id;
+    // }
+
+
+
+
+    switch(nums.target.id){
+      case "grey":
+        body.style.backgroundColor = nums.target.id;
+        break;
+
+      case "black":
+        body.style.backgroundColor = nums.target.id;
+        break;
+
+      case "blue":
+        body.style.backgroundColor = nums.target.id;
+        break;
+
+      case "yellow":
+        body.style.backgroundColor = nums.target.id;
+        break;
+
+      case "red":
+        body.style.backgroundColor = nums.target.id;
+        break;       
+
+      case "pink":
+        body.style.backgroundColor = nums.target.id;
+        break; 
+    }
+
+
+  });
+});
