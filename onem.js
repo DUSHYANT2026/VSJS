2439a1bbb79539385ae5770a088b7a2d1a5ba944
Dushyant2026 <dushyantpandit799@gmail.com>
2024-07-21 17:29:43 +0530

    mark all the changes

--- onem.js
+++ onem.js
@@ -1,16 +1,70 @@
-var temp= 34785;                                              console.log((temp));        console.log(typeof(temp)); 
-let temp1 = "king of kings";                                  console.log(temp1);         console.log(typeof(temp1)); 
-const temp2 = true;                                           console.log(temp2);         console.log(typeof(temp2)); 
-let nums = [1,32,4, "wrestle chatter"];                       console.log(nums);          console.log(typeof(nums)); 
-let obj = { 1 : "Jon-Jones" , 2: "Alex-Pereira"};             console.log(obj);           console.log(typeof(obj));  
-let temp4 = "all about the programming";                      console.log((temp4));       console.log(typeof(temp4)); 
-temp4 = 3487659342;                                           console.log((temp4));       console.log(typeof(temp4)); 
+// DAY 1:
 
-const temp5 = 498590425;                                      console.log((temp5));       console.log(typeof(temp5)); 
 
-// temp5 = false;                                        console.log((temp5));       console.log(typeof(temp5));      // this didn't give any output on the console beacuse of const is immutable 
+// var temp= 34785;                                              console.log((temp));        console.log(typeof(temp)); 
+// let temp1 = "king of kings";                                  console.log(temp1);         console.log(typeof(temp1)); 
+// const temp2 = true;                                           console.log(temp2);         console.log(typeof(temp2)); 
+// let nums = [1,32,4, "wrestle chatter"];                       console.log(nums);          console.log(typeof(nums)); 
+// let obj = { 1 : "Jon-Jones" , 2: "Alex-Pereira"};             console.log(obj);           console.log(typeof(obj));  
+// let temp4 = "all about the programming";                      console.log((temp4));       console.log(typeof(temp4)); 
+// temp4 = 3487659342;                                           console.log((temp4));       console.log(typeof(temp4)); 
 
-nums.forEach( (it) => {
-    console.log(it);
-})
-let index = nums.indexOf("wrestle chatter");          console.log(index);
+// const temp5 = 498590425;                                      console.log((temp5));       console.log(typeof(temp5)); 
+
+// // temp5 = false;                                             console.log((temp5));       console.log(typeof(temp5));      // this didn't give any output on the console beacuse of const is immutable 
+
+// nums.forEach( (it) => {
+//     console.log(it);
+// })
+// let index = nums.indexOf("wrestle chatter");                 console.log(index);
+
+
+
+
+// DAY 2:
+
+let x = 10 ;  let y = 20;
+
+let temp1 = x+y;                    console.log(temp1);
+let temp2 = x-y;                    console.log(temp2);
+let temp3 = x*y;                    console.log(temp3);
+let temp4 = x/y;                    console.log(temp4);
+let temp5 = x%y;                    console.log(temp5); 
+
+console.log("\n");
+
+const temp11 = y+x;                 console.log(temp11);
+const temp12 = y-x;                 console.log(temp12);
+const temp13 = y*x;                 console.log(temp13);
+const temp14 = y/x;                 console.log(temp14);
+const temp15 = y%x;                 console.log(temp15); 
+
+console.log("\n");
+
+let nums = [10,20,40,50,60]
+let sum = 0;
+let sub = 180;
+for(let it of nums){
+    sum += it;
+    sub -= it;
+    console.log(sum);
+    console.log(sub);
+}
+console.log("\n");
+console.log(sum);
+console.log(sub);
+
+
+console.log('\n')
+
+if(x > y){
+    console.log(x);
+}else{
+    console.log(y);
+}
+
+if(x == y){
+    console.log("TRUE");
+}else{
+    console.log("false");
+}
